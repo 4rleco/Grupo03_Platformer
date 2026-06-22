904cc2f53383d43e3e7f455277c50ced3c3f5ae7
4rleco <117652001+4rleco@users.noreply.github.com>
2026-06-22 10:40:25 -0300

    Created player controller script

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
