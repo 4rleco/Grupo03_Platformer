ae6ef15139d1d6f4a9e50721bb5646933efd713e
4rleco <117652001+4rleco@users.noreply.github.com>
2026-06-28 23:22:25 -0300

    When player falls goes back to the start point

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
@@ -8,9 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float force;
     [SerializeField] private ForceMode2D forceMode;
+    [SerializeField] private GameObject spawnPoint;
 
     private Rigidbody2D rigidbody;
     private BoxCollider2D boxCollider;
+
+    private float maxPosY = 6;
+    private float minPosY = -6;
 
     private Vector2 movement;
 
@@ -23,6 +26,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
+
+       if(transform.position.y < minPosY || transform.position.y > maxPosY)
+        {
+            transform.position = spawnPoint.transform.position;
+        }
     }
     private void FixedUpdate()
     {
