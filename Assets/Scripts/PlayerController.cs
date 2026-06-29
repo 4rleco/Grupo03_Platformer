6e21b0560c55e273e3bf66fe913b0b5e4f50123a
4rleco <117652001+4rleco@users.noreply.github.com>
2026-06-29 01:07:21 -0300

    Fixed scripts names

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -6,16 +7,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float force;
+    [SerializeField] private int lifes = 3;
     [SerializeField] private ForceMode2D forceMode;
     [SerializeField] private GameObject spawnPoint;
 
     private Rigidbody2D rigidbody;
     private BoxCollider2D boxCollider;
 
-    private float maxPosY = 6;
-    private float minPosY = -6;
+    private float maxPosY = 8;
+    private float minPosY = -8;
 
     private Vector2 movement;
+
+    public event Action<bool> OnGameOver;
 
     private void Awake()
     {
@@ -25,9 +29,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (lifes <= 0)
+            OnGameOver?.Invoke(true);
+
         Movement();
 
-       if(transform.position.y < minPosY || transform.position.y > maxPosY)
+        if (transform.position.y < minPosY || transform.position.y > maxPosY)
         {
             transform.position = spawnPoint.transform.position;
         }
@@ -35,6 +42,18 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.AddForce(movement * force, forceMode);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Saw & Spikes"))
+        {
+            lifes--;
+            transform.position = spawnPoint.transform.position;
+        }
+
+        if (collision.gameObject.CompareTag("EndPoint"))
+            OnGameOver?.Invoke(true);
     }
 
     private void Movement()
